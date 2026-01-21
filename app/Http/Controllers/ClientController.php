a969cfbfb39eba9c2ef1d61364929a3349b8fe04
Rafanambinantsoa <akutagawakarim@gmail.com>
2026-01-21 09:50:52 +0300

    dsds

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\Client;
+use Illuminate\Container\Attributes\DB;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
